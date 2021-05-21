01a701f95d1d01c0106716bf63732de94908dfb8
Felmachersoft <contact@felmachersoft.de>
2021-05-21 22:57:11 +0200

    Added support for just-in-time chat log download, emotes and badges

--- src/utils/MPVClient.ts
+++ src/utils/MPVClient.ts
@@ -1,7 +1,25 @@
-import { access, createReadStream } from 'fs';
+import { access, createReadStream, readFile } from 'fs';
 import { createInterface } from 'readline';
 import { remote } from 'electron';
 import mpv from 'mpv-ipc';
+
+interface IStoredMessage {
+  content: string;
+  channel_id: string;
+  author_displayname: string;
+  color: string;
+  emotes: {
+    _id: string;
+    begin: number;
+    end: number;
+  }[];
+  badges: {
+    _id: string;
+    version: string;
+  }[];
+  datetime: Date;
+  offset: number;
+}
 
 export default class MPVClient {
   private messages: any[] = [];
@@ -25,60 +43,137 @@ export default class MPVClient {
 
   init(chatlog, socketName = 'MPVControllPipe'): Promise<void> {
     return new Promise((resolve, reject) => {
-      access(chatlog, (err) => {
-        if (err) {
-          reject(err);
-        }
-        const readInterface = createInterface({
-          input: createReadStream(chatlog),
-        });
-        let startDate;
-        let i = 0;
-        readInterface.on('line', (line) => {
-          let elements = line.split(' ');
-          const [day, month, year] = elements[0].split('.').map((e) => parseInt(e, 10));
-          const [hours, minutes, seconds] = elements[1].split(':').map((e) => parseInt(e, 10));
-          const messageDate = new Date(year, month - 1, day, hours, minutes, seconds);
-          if (i === 0) {
-            startDate = messageDate;
-          }
-          // Divide by 1000 to get seconds
-          const time = (messageDate.getTime() - startDate.getTime()) / 1000;
-          elements = line.split(': ');
-          // eslint-disable-next-line
-          const username = elements.shift()!.split(' - ')[1];
-          const message = elements.join();
-
-          this.messages.push({
-            time,
-            message,
-            tags: {
-              username,
-              color: null,
-              emotes: null,
-              badges: null,
-            },
+      const callback = () => {
+        const socketPrefix = '\\\\.\\pipe\\';
+        const socket = socketPrefix + socketName;
+        this.player = new mpv.MPVClient(socket);
+        this.handleSeek();
+        this.player.on('close', this.disconnect.bind(this));
+        this.player.on('seek', this.handleSeek.bind(this));
+        if (chatlog === 'ondemand') {
+          // this.player.on('chatmessages', (e) => this.handleReceivedMessages(e));
+          this.player.on('chatmessages', (e) => {
+            this.handleReceivedMessages(e);
           });
-          i++;
+        }
+        this.intervalHandle = setInterval(this.checkForAndEmitNewMessages.bind(this), 2000);
+        resolve();
+      };
+
+      if (chatlog === 'ondemand') {
+        callback();
+      } else {
+        access(chatlog, (err) => {
+          if (err) {
+            reject(err);
+          }
+
+          if (chatlog) {
+            if (chatlog.endsWith('txt')) this.loadMessagesFromTxt(chatlog, callback);
+
+            if (chatlog.endsWith('json')) this.loadMessagesFromJson(chatlog, callback);
+          }
         });
-        readInterface.on('close', () => {
-          const socketPrefix = '\\\\.\\pipe\\';
-          const socket = socketPrefix + socketName;
-          this.player = new mpv.MPVClient(socket);
-          this.handleSeek();
-          this.player.on('close', this.disconnect.bind(this));
-          this.player.on('seek', this.handleSeek.bind(this));
-          this.intervalHandle = setInterval(this.checkForAndEmitNewMessages.bind(this), 2000);
-          resolve();
-        });
-      });
+      }
     });
   }
 
   async disconnect(): Promise<void> {
     clearInterval(this.intervalHandle);
     this.player.off('seek', this.handleSeek.bind(this));
+    // this.player.off('chatmessages', this.handleReceivedMessages.bind(this));
     remote.getCurrentWindow().close();
+  }
+
+  private loadMessagesFromTxt(chatlog: string, callback: () => void) {
+    const readInterface = createInterface({
+      input: createReadStream(chatlog),
+    });
+    let startDate: Date;
+    let i = 0;
+    readInterface.on('line', (line) => {
+      let elements = line.split(' ');
+      const [day, month, year] = elements[0].split('.').map((e) => parseInt(e, 10));
+      const [hours, minutes, seconds] = elements[1].split(':').map((e) => parseInt(e, 10));
+      const messageDate = new Date(year, month - 1, day, hours, minutes, seconds);
+      if (i === 0) {
+        startDate = messageDate;
+      }
+      // Divide by 1000 to get seconds
+      const time = (messageDate.getTime() - startDate.getTime()) / 1000;
+      elements = line.split(': ');
+      // eslint-disable-next-line
+      const username = elements.shift()!.split(' - ')[1];
+      const message = elements.join();
+
+      this.messages.push({
+        time,
+        message,
+        tags: {
+          username,
+          color: null,
+          emotes: null,
+          badges: null,
+        },
+      });
+      i++;
+    });
+
+    readInterface.on('close', callback);
+  }
+
+  private loadMessagesFromJson(chatlog: string, callback: () => void) {
+    readFile(chatlog, 'utf8', (err, data) => {
+      if (err) {
+        // TODO handle error
+        console.error(`Failed to read file: ${err}`);
+      }
+
+      let storedMgs: IStoredMessage[] = [];
+
+      try {
+        storedMgs = JSON.parse(data);
+      } catch (error) {
+        // TODO handle error
+        console.error(`Error while parsing json file: ${error}`);
+      }
+
+      this.handleNewMessages(storedMgs);
+
+      callback();
+    });
+  }
+
+  private stored2ChatMessage(storedMsg: IStoredMessage) {
+    const wellformedEmotes: {} = {};
+    if (storedMsg.emotes) {
+      for (const emote of storedMsg.emotes) {
+        if (!(emote._id in wellformedEmotes)) wellformedEmotes[emote._id] = [];
+        wellformedEmotes[emote._id].push(`${emote.begin}-${emote.end}`);
+      }
+    }
+
+    const wellformedBadges: {} = {};
+    if (storedMsg.badges) {
+      for (const badge of storedMsg.badges) {
+        if (!(badge._id in wellformedBadges)) {
+          wellformedBadges[badge._id] = badge.version;
+        }
+      }
+    }
+
+    return {
+      time: storedMsg.offset,
+      message: storedMsg.content,
+      channel_id: storedMsg.channel_id,
+      tags: {
+        username: storedMsg.author_displayname,
+        color: storedMsg.color,
+        emotes: wellformedEmotes,
+        badges: wellformedBadges,
+        'room-id': storedMsg.channel_id,
+      },
+    };
   }
 
   private checkForAndEmitNewMessages() {
@@ -103,6 +198,14 @@ export default class MPVClient {
       this.emit('delete', []);
       this.checkForAndEmitNewMessages();
     });
+  }
+
+  private handleReceivedMessages(eve: { event: string; data: IStoredMessage[] }) {
+    this.handleNewMessages(eve.data);
+  }
+
+  private handleNewMessages(storedMgs: IStoredMessage[]) {
+    for (const storedMsg of storedMgs) this.messages.push(this.stored2ChatMessage(storedMsg));
   }
 
   private emit(event: string, data: any) {
